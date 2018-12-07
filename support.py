ae5153f468b06ef49babf84f94324bcaf667f07b
duncanlindsey <duncan.m.lindsey@gmail.com>
2018-12-07 02:28:00 +0000

    It's fucking greedy and minima

--- support.py
+++ support.py
@@ -1,7 +1,65 @@
 import random
+from scipy.special import comb
+from scipy.stats import binom
 
 def gen_random(towers, troops):
     army = [0 for x in range(towers)]
     for x in range(troops):
         army[random.randint(0,towers-1)] += 1
     return army
+
+def fight(army1, army2):
+    if len(army1) == len (army2):
+        points1 = 0
+        points2 = 0
+        for battle in range(len(army1)):
+            if army1[battle] > army2[battle]:
+                points1 += battle + 1
+            elif army1[battle] < army2[battle]:
+                points2 += battle + 1
+        return (points1, points2)
+
+def gen_enemies(players, towers, troops, method):
+    methods = {'random': gen_random}
+    npcs = [0 for x in range(players)]
+    for x in range(players):
+        npcs[x] = methods[method](towers, troops)
+    return npcs
+
+def brawl(player_army, enemies, method):
+
+    #Extract brawl parameters from player army
+    towers = len(player_army)
+    troops = sum(player_army)
+
+    #Build enemy armies and combine with player army
+    npcs = gen_enemies(enemies, towers, troops, method)
+    brawl = [player_army, *npcs]
+
+    #Run a "everyone players everyone once" battle
+    scores = [0 for x in range(len(brawl))]
+    for player in range(len(scores)):
+        for opponent in range(player + 1, len(scores)):
+            #print ("Player %s versus Player %s" % (player, opponent))
+            results = fight(brawl[player],brawl[opponent])
+            #print ("The score is %s!" % (str(results)))
+            scores[player] += results[0]
+            scores[opponent] += results[1]
+    return rank_score(scores[0], scores)
+
+def rank_score(my_score, all_scores):
+    sorted_scores = sorted(all_scores, reverse = True)
+    return sorted_scores.index(my_score) + 1
+
+def simulation(army, enemies, method, iterations):
+    rank_distribution = [0 for x in range(enemies+1)]
+    for x in range(iterations):
+        rank_distribution[brawl(army, enemies, method)-1] += 1
+    return rank_distribution
+
+#def binom(towers, k, troops):
+    #return comb(troops, k) * ((1/towers)**k) * ((1-(1/towers))**(troops - k))
+
+def binom_dist(towers, troops):
+    rv = binom(troops, 1/towers)
+    return [rv.pmf(k) for k in range(0, troops + 1)]
--- working.py
+++ working.py
@@ -1,4 +1,26 @@
 import support as sup
+import matplotlib.pyplot as plt
 
-army = sup.gen_random(3,10)
-print (army)
+def hill_climb(towers, troops):
+    dist = sup.binom_dist(towers, troops)
+    print (dist)
+    strategy = [0 for x in range(towers)]
+    for x in range(troops):
+        deltas = [(i+1)*dist[strategy[i]] for i in range(towers)] #Evaluate adding a troop to each tower
+        strategy[deltas.index(max(deltas))] += 1
+        #print (deltas)
+        print (strategy)
+    return strategy
+
+my_army = hill_climb(7,19)
+#my_army = [0,3,7]
+print(sup.simulation(my_army, 9, 'random', 10000))
+
+##npcs = sup.gen_enemies(10000, 5, 15, 'random')
+#plt.hist([i[0] for i in npcs])
+#plt.show()
+
+#print (hill_climb(5, 15))
+#print (sup.binom_dist(5,15))
+
+
